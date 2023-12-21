af1a73fff518e0995747ed8a7cb2b287883cb075
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2023-12-22 02:24:50 +0900

    feat:게시글,상단카테고리 테스트

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Github blog 시작합니다 :)",
+        "excerpt":"제목 테스트 h1   제목 테스트 h2   코드블럭 테스트  테스트  ","categories": ["Spring"],
+        "tags": ["Blog","jekyll","Github","Git"],
+        "url": "/spring/first/",
+        "teaser": null
+      }]
