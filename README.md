3fac6c8a74f28f8c807cafc97b3e18e4e989c24e
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2023-12-22 13:18:21 +0900

    feat: 23.12.22

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 ## 공부기록 블로그 입니다😊
+
+Back-end개발자가 되기 위해 공부하고 기록하는 블로그 입니다.  
+설명은 향후에 추가 예정!!
+
+
+- 블로그를 설정할 때 참고한 링크
+
+    - [도각도각 Dev DAy](https://devyuseon.github.io/)
+
+    - [평생 공부 블로그: Today I Learned](https://ansohxxn.github.io/)
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "[2023-12-22 TIL]",
-        "excerpt":"오늘의 학습     프로그래머스 레벨2 2문제 풀이            최솟값 만들기(https://school.programmers.co.kr/learn/courses/30/lessons/12941)       올바른 괄호(https://school.programmers.co.kr/learn/courses/30/lessons/12909)           새로 알게 된 사실     String의 경우 .equal(“A”)로 비교하는데 char의 경우 == ‘A’로 비교하게 된다.     1 2 String.equal(\"A\") char == 'A'          ","categories": ["TIL"],
+        "title": "[2023-12-22]TIL",
+        "excerpt":"오늘의 학습     프로그래머스 레벨2 2문제 풀이            최솟값 만들기       올바른 괄호           새로 알게 된 사실     String의 경우 .equal(“A”)로 비교하는데 char의 경우 == ‘A’로 비교하게 된다.     1 2 String.equal(\"A\") char == 'A'          ","categories": ["TIL"],
         "tags": ["TIL","Java"],
         "url": "/til/TIL/",
         "teaser": null
